d82848a6416de029c3818f62fd57cf827c22eb35
Felipe Toro <fitsto@gmail.com>
2012-06-03 02:47:12 -0400

    agregado el codigo jquery para cargar contenido
    se agrego el codigo jquery para agregar contenido dentro de un div
    content

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
 /* Author:
-
+	Felipe Toro
 */
+$(document).ready(function() {
+						   
+	var hash = window.location.hash.substr(1);
+	var href = $('nav a').each(function(){
+		var href = $(this).attr('href');
+		if(hash==href.substr(0,href.length-5)){
+			var toLoad = hash+'.html #content';
+			$('#content').load(toLoad)
+		}											
+	});
 
+	$('nav a').click(function(){
+								  
+		var toLoad = $(this).attr('href')+' #content';
+		$('#content').hide(10,loadContent);
+		$('#load').remove();
+		$('#carga').append('<span id="load">LOADING...</span>');
+		$('#load').fadeIn('normal');
+		window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+		function loadContent() {
+			$('#content').load(toLoad,'',showNewContent())
+		}
+		function showNewContent() {
+			$('#content').show(10,hideLoader());
+		}
+		function hideLoader() {
+			$('#load').fadeOut('normal');
+		}
+		return false;
+		
+	});
 
-
+});
 
 
 
